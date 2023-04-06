40bcf857ee2962b907bf5e838c7facdd41f85422
Bogdan Stefania <bos1clj@bosch.com>
2023-04-06 17:02:07 +0300

    semantic elements for sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -43,41 +43,41 @@ function Sidebar() {
         <hr />
       </div>
       <div className="content">
-        <div className="pages">
-          <div className="pageRef">
+        <ul className="pages">
+          <li className="pageRef">
             <FontAwesomeIcon icon={faHouse} className="formIcon" />
             {showTitle && <p>Home</p>}
-          </div>
-          <div className="pageRef">
+          </li>
+          <li className="pageRef">
             <FontAwesomeIcon icon={faWallet} className="formIcon" />
             {showTitle && <p>Wallet</p>}
-          </div>
-          <div className="pageRef">
+          </li>
+          <li className="pageRef">
             <FontAwesomeIcon icon={faCreditCard} className="formIcon" />
             {showTitle && <p>Spendings</p>}
-          </div>
-          <div className="pageRef">
+          </li>
+          <li className="pageRef">
             <FontAwesomeIcon icon={faPiggyBank} className="formIcon" />
             {showTitle && <p>Savings</p>}
 
-          </div>
-          <div className="pageRef">
+          </li>
+          <li className="pageRef">
             <FontAwesomeIcon icon={faMoneyBillTrendUp} className="formIcon" />
             {showTitle && <p className=''>Investments</p>}
-          </div>
-          <div className="pageRef">
+          </li>
+          <li className="pageRef">
             <FontAwesomeIcon icon={faUserGroup} className="formIcon" />
             {showTitle && <p>Friends</p>}
-          </div>
-          <div className="pageRef">
+          </li>
+          <li className="pageRef">
             <FontAwesomeIcon icon={faGears} className="formIcon" />
             {showTitle && <p>Settings</p>}
-          </div>
-          <div className="pageRef">
+          </li>
+          <li className="pageRef">
             <FontAwesomeIcon icon={faCalendarDays} className="formIcon" />
             {showTitle && <p>Calendar</p>}
-          </div>
-        </div>
+          </li>
+        </ul>
         <FontAwesomeIcon icon={faCircleUser} className="formIcon" />
       </div>
     </div>
