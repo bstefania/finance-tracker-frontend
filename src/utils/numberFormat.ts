39b07657d8a6333ae20e8da68cd23fee8f795669
Stefania Bogdan <bogdan_stefania11@yahoo.com>
2023-06-25 13:29:04 +0300

    fix currency round up

--- src/utils/numberFormat.ts
+++ src/utils/numberFormat.ts
@@ -1,13 +1,11 @@
 export const euro = Intl.NumberFormat(undefined, {
   style: "currency",
   currency: "EUR",
-  maximumSignificantDigits: 2,
 });
 
 export const ron = Intl.NumberFormat(undefined, {
   style: "currency",
   currency: "RON",
-  maximumSignificantDigits: 2,
 });
 
 export const formatDecimals = (number: number | undefined) => {
