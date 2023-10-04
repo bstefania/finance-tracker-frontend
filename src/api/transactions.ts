a13b94ef47c9537f5d32f78463ed4c6d288a4287
Stefania Bogdan <bogdan_stefania11@yahoo.com>
2023-10-04 21:18:02 +0300

    get amounts from current month

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -3,9 +3,13 @@ import { customAxios } from "./axios";
 export type Amounts = Record<string, Record<string, {amount: number, color: string}>>
 
 export const getAmounts = async () => {
+  const currentDate = new Date();
+  const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  
   const res = await customAxios.get("/amounts", {
     params: {
-      type: "expense"
+      type: "expense",
+      createdAtStart: firstDayOfMonth.toString()
     }
   })
   return res.data.data as Amounts;
