97895938a712d1352f2bf3862f56c51dc0cbe511
Stefania Bogdan <bogdan_stefania11@yahoo.com>
2023-10-19 19:48:38 +0300

    code improvement external provider component

--- src/components/utils/ExternalProvider.tsx
+++ src/components/utils/ExternalProvider.tsx
@@ -1,27 +1,30 @@
-import React from "react";
-import "../../styles/utils/ExternalProvider.scss"
+import "../../styles/utils/ExternalProvider.scss";
 import { AuthProviderType } from "../../types/authentication";
 import { signInWithThirdParty } from "../../utils/authentication";
 
-function externalProvider() {
+const externalProvider = () => {
+  const options = [
+    {
+      type: AuthProviderType.Google,
+      img: "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg",
+    },
+    {
+      type: AuthProviderType.Facebook,
+      img: "https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg",
+    },
+  ];
+
   return (
     <div className="external-provider">
-      <div className="external-provider-button" onClick={() => signInWithThirdParty(AuthProviderType.Google)}>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-          className="provider-icon"
-          alt=""
-        />
-        <span className="text-container">Continue with Google </span>
-      </div>
-      <div className="external-provider-button" onClick={() => signInWithThirdParty(AuthProviderType.Facebook)}>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg"
-          className="provider-icon"
-          alt=""
-        />
-        <span className="text-container">Continue with Facebook </span>
-      </div>
+      {options.map((option) => (
+        <div
+          className="external-provider-button"
+          onClick={() => signInWithThirdParty(option.type)}
+        >
+          <img src={option.img} className="provider-icon" alt="" />
+          <span className="text-container">Continue with {option.type}</span>
+        </div>
+      ))}
     </div>
   );
 }
