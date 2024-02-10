1dd2997d714d3003d2373d8d5a1466919bb8b431
Stefania Bogdan <bogdan_stefania11@yahoo.com>
2024-02-10 19:36:52 +0200

    button improvement

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,18 +1,32 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import styles from "../../styles/atoms/Button.module.scss";
+import Icon from "./Icon";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   primary?: boolean;
   secondary?: boolean;
+  tertiary?: boolean;
+  icon?: string;
 };
 
-function Button({ primary, secondary, children, ...extraProps }: ButtonProps) {
+function Button({ primary, secondary, tertiary, icon, children, ...extraProps }: ButtonProps) {
   let classList = [extraProps.className || ""];
   if (secondary) {
     classList.push(styles["button--secondary"]);
   }
+  if (tertiary) {
+    classList.push(styles["button--tertiary"]);
+  }
+  if (icon && !children) {
+    classList.push(styles["button--icon"]);
+  }
+
   const className = classList.join(" ");
 
-  return <button className={className} {...extraProps}>{children}</button>;
+  return <button className={className} {...extraProps}>
+    {icon && <Icon icon={icon as IconProp} />}
+    {children}
+    </button>;
 }
 
 export default Button;
